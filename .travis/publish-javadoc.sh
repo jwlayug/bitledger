e578236f34b2814f4b4e5e95691ebe4582b86c50
Chris Beams <chris@beams.io>
2014-06-05 18:25:35 +0200

    Fix Javadoc publication

--- .travis/publish-javadoc.sh
+++ .travis/publish-javadoc.sh
@@ -14,12 +14,18 @@ if [ "$TRAVIS_REPO_SLUG" == "cbeams/bitledger" ] \
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
 
+    cd $HOME
+    git clone --quiet --branch=master https://${GH_TOKEN}@github.com/cbeams/bitledger bitledger-javadoc > /dev/null
+
+    cd bitledger-javadoc
+    ./gradlew javadoc
+
     # Create a fresh clone in which to publish Javadoc to gh-pages
     cd $HOME
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cbeams/bitledger gh-pages > /dev/null
-    cd gh-pages
-    git rm -rf ./api
-    cp -Rf $HOME/api-temp ./api
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cbeams/bitledger bitledger-gh-pages > /dev/null
+    cd bitledger-gh-pages
+    git rm -rf --ignore-unmatch ./api
+    cp -Rf $HOME/bitledger-javadoc/build/docs/javadoc ./api
     git add -f .
     git commit -m "Publish Javadoc from Travis CI build $TRAVIS_BUILD_NUMBER"
     git push -fq origin gh-pages > /dev/null
